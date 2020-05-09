2d7dfe66fc9aa742d8f0001fb0dfa8ed794d1c57
lalit@lalitadithya.com <lalit@lalitadithya.com>
2020-05-09 18:40:00 +0530

    Added models

---
+++ src/PlanetScaleTodoApp.Functions/Models/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanetScaleTodoApp.Functions.Models
+{
+    public class TodoItem
+    {
+        public Guid Id { get; set; }
+        public string Item { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
---
+++ src/PlanetScaleTodoApp.Functions/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanetScaleTodoApp.Functions.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public List<TodoItem> Items { get; set; }
+    }
+}
